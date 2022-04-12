af41b3471412400c409289dcd3d284533ff039b9
JuniorDeveloperrr <103493877+JuniorDeveloperrr@users.noreply.github.com>
2022-04-12 12:18:38 +0300

    Add files via upload

---
+++ ucgenalanhesaplamaaracı.py
@@ -0,0 +1,7 @@
+print("Üçgen Alan Hesaplama Aracına Hoşgeldiniz")
+
+alttaban = input("Lütfen yüksekliğin inmiş olduğu tabanın uzunluğunu giriniz.Veya daha anlaşılır bir dille alttaban uzunluğunu giriniz.")
+h = input("Lütfen yüksekliği giriniz")
+
+print(int(alttaban)*int(h)/2)
+
